8782c932b883e241559c52dd7e23e1287ac7ebbc
Saif Ullah <muhammadalisaif785@gmail.com>
2025-09-27 20:52:39 +0500

    created using vs code
    while working on Project_01_House_Price_Prediction_using_ANN

--- Project_01_House_Price_Prediction_using_ANN/app.py
+++ Project_01_House_Price_Prediction_using_ANN/app.py
@@ -2,3 +2,15 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 from tensorflow.keras.models import load_model
+
+
+# Now you can use this model to make predictions
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Load the model and scaler using pickle
+# Load the model
+model = load_model('model_ann.h5')
+with open('scaler.pkl', 'rb') as scaler_file:
+    scaler = pickle.load(scaler_file)
